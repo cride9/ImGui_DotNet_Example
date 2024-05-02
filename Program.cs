afa47293b0377f7632e13239fcf553319ca1c6dc
cride <gamingbalint80@gmail.com>
2024-05-02 15:41:29 +0200

    letsgo

--- Program.cs
+++ Program.cs
@@ -1,64 +1,54 @@
-﻿using ImGuiNET;
+﻿using System.Runtime.CompilerServices;
 using ClickableTransparentOverlay;
+using Newtonsoft.Json;
 using System.Numerics;
-using System.Runtime.CompilerServices;
+using System.Drawing;
+using ImGuiNET;
 
 namespace ImGuiC_ {
     public class Program : Overlay {
-        static ImColor childColor = new( ) { Value = new Vector4( 0.15f, 0.15f, 0.15f, 1f ) };
-        static ImColor backgroundColor = new( ) { Value = new Vector4( 0.1f, 0.1f, 0.1f, 1f ) };
-        static ImColor borderColor = new( ) { Value = new Vector4( 0.3f, 0.3f, 0.3f, 1f ) };
 
-        bool checkbox1 = false;
+        Vector4 temp = new( );
+        int currentStyleColor = 0;
+        static List<StringVectorPair> ImColStrings;
 
-        protected override void Render( ) {
+        protected unsafe override void Render( ) {
+
+            // this shows a full detailed and modifyable imgui demo window
+            // NOTE: Would be good to do an universal class to save the demo window "ImGuiStyleVars" for easier development
+            //ImGui.ShowDemoWindow( );
 
             PushStyles( );
-            ImGui.Begin( "Window" );
+            ImGui.Begin( "Window##1", ImGuiWindowFlags.NoTitleBar );
             {
-                ImGui.BeginChild( "##id1", new Vector2( ImGui.GetContentRegionAvail().X / 2, ImGui.GetContentRegionAvail().Y ), ImGuiChildFlags.Border );
-                {
-                    ImGui.Checkbox( "checkbox##1", ref checkbox1 );
-                    ImGui.ColorEdit4( "Border##1", ref borderColor.Value, ImGuiColorEditFlags.NoInputs | ImGuiColorEditFlags.NoSidePreview );
-                    ImGui.SameLine( );
-                    ImGui.ColorEdit4( "Window background##2", ref backgroundColor.Value, ImGuiColorEditFlags.NoInputs | ImGuiColorEditFlags.NoSidePreview );
-                    ImGui.ColorEdit4( "Child background##2", ref childColor.Value, ImGuiColorEditFlags.NoInputs | ImGuiColorEditFlags.NoSidePreview );
-                }
-                ImGui.EndChild( );
-                ImGui.SameLine( );
-                ImGui.BeginChild( "##id2", new Vector2( ImGui.GetContentRegionAvail( ).X, ImGui.GetContentRegionAvail( ).Y ), ImGuiChildFlags.Border );
-                {
-                    ImGui.Checkbox( "checkbox##2", ref checkbox1 );
-                    ImGui.BeginListBox( "#listbox" );
-                    {
-                        for ( int i = 0; i < (int)ImGuiStyleVar.COUNT; i++ ) {
-                            ImGui.Selectable( ( ImGuiStyleVar )i, false);
+                // draw listbox with the color variables
+                ImGui.ListBox( "##listbox", ref currentStyleColor, ImColStrings.Select( select => select.Name ).ToArray( ), ImColStrings.Count );
 
-                        }
-                    }
-                    ImGui.EndListBox( );
-                }
-                ImGui.EndChild( );
+                // get the current color variable based on the selected color
+                // NOTE: This functions returns a pointer to the color so we have to dereference it
+                temp = *ImGui.GetStyleColorVec4( ( ImGuiCol )currentStyleColor );
+
+                // draw the color picker with a temp color
+                // NOTE: temp color is needed becuase of the CS1510 error (A ref or out value must be an assignable variable)
+                ImGui.ColorEdit4( "##color", ref temp, ImGuiColorEditFlags.NoInputs | ImGuiColorEditFlags.NoSidePreview | ImGuiColorEditFlags.AlphaBar );
+
+                // Set the current selected color
+                ImColStrings[ ( int )currentStyleColor ] = new( Enum.Parse<ImGuiCol>( currentStyleColor.ToString( ) ).ToString( ), temp);
+
+                // Make the json saving here :D
+                if ( ImGui.Button( "Save color config", new Vector2( ImGui.GetContentRegionAvail( ).X, 30 ) ) )
+                    SerializeToJson( ImColStrings );
             }
             ImGui.End( );
-
-            ImGui.Begin( "Window##2" );
-            {
-                if ( ImGui.Button( "Heloka nyomj meg" ) )
-                    Console.WriteLine( "Megnyomtad!" );
-            }
-            ImGui.End( );
-
-            ImGui.PopStyleColor( );
             PopStyles( );
         }
 
         void PushStyles() {
 
-            // colors
-            ImGui.PushStyleColor( ImGuiCol.ChildBg, childColor.Value );
-            ImGui.PushStyleColor( ImGuiCol.WindowBg, backgroundColor.Value );
-            ImGui.PushStyleColor( ImGuiCol.Border, borderColor.Value );
+            // Push colors that are set
+            foreach ( var item in ImColStrings )
+                if ( item.Vector is not null )
+                    ImGui.PushStyleColor( Enum.Parse<ImGuiCol>( item.Name ), (Vector4)item.Vector );
 
             // styles
             ImGui.PushStyleVar( ImGuiStyleVar.WindowMinSize, new Vector2( 500, 300 ) );
@@ -72,17 +62,51 @@ namespace ImGuiC_ {
 
         void PopStyles( ) {
 
-            ImGui.PopStyleColor( 2 );
+            // Pop all the styles
+            ImGui.PopStyleColor( ImColStrings.Count( where => where.Vector is not null ) );
             ImGui.PopStyleVar( 7 );
         }
 
         public static void Main( string[] args ) {
 
-            Console.WriteLine( "Starting..." );
+            // create if cfg not exists
+            if ( !File.Exists( @"colorsSave.json" ) )
+                File.Create( @"colorsSave.json" ).Close();
 
+            // load from json if it has any data
+            ImColStrings = DeserializeFromJson( );
+
+            // bebebe wrong data check
+            if ( ImColStrings is null ) {
+                ImColStrings = new( );
+                for ( ImGuiCol i = ImGuiCol.Text; i < ImGuiCol.COUNT; i++ )
+                    ImColStrings.Add( new( i.ToString( ), null ) );
+            }
+
+            // create imgui window :D
             Program _this = new( );
             _this.Start( ).Wait( );
-            _this.Size = new System.Drawing.Size( 1920, 1080 );
+            _this.Size = new Size( 1920, 1080 );
+        }
+
+        // saving to json
+        public static void SerializeToJson( List<StringVectorPair> list ) =>
+            File.WriteAllText( @"colorsSave.json", JsonConvert.SerializeObject( list, Formatting.Indented ) );
+        
+        // load from json
+        public static List<StringVectorPair>? DeserializeFromJson( ) =>
+            JsonConvert.DeserializeObject<List<StringVectorPair>>( File.ReadAllText( @"colorsSave.json" ) );
+        
+    }
+
+    // json save file
+    public struct StringVectorPair {
+        public string Name { get; set; }
+        public Vector4? Vector { get; set; }
+
+        public StringVectorPair( string _name, Vector4? _vector ) {
+            Name = _name;
+            Vector = _vector;
         }
     }
 }
